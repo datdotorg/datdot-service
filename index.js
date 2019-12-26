55bb20e5500879c7df7a2acef3d072ae3ef1cfde
ninabreznik <ninabreznik@gmail.com>
2019-12-27 03:04:36 +0700

    Cleanup and add comments

--- index.js
+++ index.js
@@ -3,65 +3,75 @@ const provider = new WsProvider('ws://127.0.0.1:9944')
 const fs = require('fs')
 const types = JSON.parse(fs.readFileSync('./src/types.json').toString())
 const hypercore = require('hypercore')
-let archive = []
 var feed = hypercore('./tmp', {valueEncoding: 'json'})
 
-feed.append({
-  hello: 'world'
-})
+// feed.append({
+//   hello: 'world'
+// })
+//
+// feed.append({
+//   hej: 'verden'
+// })
+//
+// feed.append({
+//   hola: 'mundo'
+// })
+//
+// console.log(feed)
 
-feed.append({
-  hej: 'verden'
-})
+start()
+async function start () {
 
-feed.append({
-  hola: 'mundo'
-})
-
-feed.ready(() => {
-  archive.push(feed.key.toString('hex'))
-  //archive[0] = ed25519::Public
-  feed.rootHashes(0, (err, res) => {
-    if (err) console.log(err)
-    archive.push({
-      hashType: 2,
-      children: [res[0].hash]
-    })
-    // archive[2] = {
-    //   hash_type: u8, //2
-    //   children: Vec<ParentHashInRoot>
-    // }
-  })
-  feed.signature((err, res) => {
-    if (err) console.log(err)
-    archive.push(res.signature.toString('hex'))
-    //archive[1] = ed25519::Signature
-  })
-})
-
-start(archive)
-async function start (archive) {
   const api = await ApiPromise.create({
     provider,
     types
   })
 
-  const keyring = new Keyring({ type: 'sr25519' });
-  const ALICE = keyring.addFromUri('//Alice');
-  console.log(archive)
+  /*----------  ACCOUNT ------------ */
+
+  // default accounts in dev env (ALICE, CHARLIE, DAVE, FERDIE, EVE etc.)
+  const keyring = new Keyring({ type: 'sr25519' })
+  const CHARLIE = keyring.addFromUri('//Charlie')
+
+  // @TODO => create new custom account <= needs ballance in current model
+  //  use library to generate mnemonic phrase
+  // const PHRASE = 'entire material egg meadow latin bargain dutch coral blood melt acoustic thought'
+  // const NEW_ACCOUNT = keyring.addFromUri(PHRASE)
+
+
+  /*----------  EXTRINSICS ------------ */
+
+// ALL extrinsics in dat_verify module
   const datVerify = api.tx.datVerify
-  .registerData(archive)
-  //.registerSeeder()
-  .signAndSend(ALICE, ({ events = [], status }) => {
-      console.log(`Current status is ${status.type}`);
 
-      if (status.isFinalized) {
-        console.log(`Transaction included at blockHash ${status.asFinalized}`);
+  // .registerSeeder() - extrinsic to register as a seeder
+  const registerSeeder = api.tx.datVerify.registerSeeder()
+  const hash_registerSeeder = await registerSeeder.signAndSend(CHARLIE)
+  console.log(hash_registerSeeder)
 
-        events.forEach(({ phase, event: { data, method, section } }) => {
-          console.log(`\t' ${phase}: ${section}.${method}:: ${data}`);
-        });
-      }
-    });
+  // .registerData(archive) - extrinsic to publish hypercore you want to be seeded
+  const registerData = api.tx.datVerify.registerData(getArchive())
+  const hash_registerData = await registerData.signAndSend(CHARLIE)
+  console.log(hash_registerData)
+
+  function getArchive () {
+    let archive = []
+    feed.ready(() => {
+      archive.push(feed.key.toString('hex')) // ed25519::Public
+      feed.rootHashes(0, (err, res) => {
+        if (err) console.log(err)
+        archive.push({
+          hashType: 2, // u8
+          children: [res[0].hash] //  Vec<ParentHashInRoot>
+        })
+      })
+      feed.signature((err, res) => {
+        if (err) console.log(err)
+        archive.push(res.signature.toString('hex')) // ed25519::Signature
+      })
+    })
+    return archive
+  }
+
 
 }
