e589ab91fa4da4c9f8a9fa9ba057f03be7cbf742
David Hawig <davidhawig@gmail.com>
2020-07-19 10:27:33 +0200

    Update cli.js
    I changed substrate to datdot-substrate since you clone git clone https://github.com/playproject-io/datdot-substrate.git

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 process.env.DEBUG = '*,-hypercore-protocol'
 
-const command1 = path.join(__dirname, '../substrate/target/release/datdot-node')
+const command1 = path.join(__dirname, '../datdot-substrate/target/release/datdot-node')
 const child1 = spawn(command1, ['--dev'], { stdio: 'inherit' })
 
 const args = [path.join(__dirname, 'lab/scenarios/mvp-1.js')] // work in progress
